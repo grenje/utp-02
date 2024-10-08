7a381f43f69cf2f57df5e6c1ac8ddeabdaacc459
grenje <s31157@pjwstk.edu.pl>
2024-10-08 12:49:18 +0200

    Specified who adds 'Adder' and who adds 'Subtractor' classes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 // TODO: we need to add the missing classes!
 
+// OK, I will add 'Adder' and s11111 will add 'Subtractor'.
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
 
-        Subtractor subtractor = new Sunbtractor();
+        Subtractor subtractor = new Subtractor();
 
         System.out.println(subtractor.subtract(6, 3));
 
